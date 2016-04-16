3dc0691c328197bac2686242f7444eec70b0917a
ReyPanda <josuhv@gmail.com>
2016-04-16 22:00:41 +0200

    Added files via upload

---
+++ Hundir la fota/Hundir la fota/Barco.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hundir_la_fota
+{
+    class Barco
+    {
+        public Barcos Tipo;
+        public Direcciones OrientacionBarco;
+        public int PosicionX;
+        public int PosicionY;
+        public bool Hundido;
+    }
+}
